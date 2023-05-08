37aae8889577e57360fd3b6bbf60a005cd2b04fa
Jhon Duarte <jhonny.jdt18@gmail.com>
2023-05-08 11:53:45 +0200

    Calculando la mediana en un array sabiendo su index con ayuda del metodo floor

--- platzimath.js
+++ platzimath.js
@@ -12,3 +12,25 @@ function calcularPromedio (lista){
         console.log(promedio);
 
 }
+
+function esPar(lista){
+    return !(lista.length % 2);
+}
+function esImpar(){
+    return lista.length % 2;
+}
+
+function calcularMediana(lista){
+
+    const listaEsPar = esPar(lista);
+
+    if(listaEsPar){
+
+    }else{
+        const indexMitadListaImpar = Math.floor(lista.length/2);
+        const medianaListaImpar = lista[indexMitadListaImpar];
+        console.log(indexMitadListaImpar);
+        console.log(medianaListaImpar);
+        return medianaListaImpar;
+    }
+}
