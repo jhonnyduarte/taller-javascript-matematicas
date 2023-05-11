b95a3df50d25964cd49599f925542df15d297c73
Jhon Duarte <jhonny.jdt18@gmail.com>
2023-05-11 23:29:46 +0200

    Creando un nuevo objeto con base en la informacion de un array

--- analisis.js
+++ analisis.js
@@ -59,3 +59,28 @@ function proyeccionPorPersona (nombrePersona){
     return nuevoSalario;
     
 }
+
+//Analisis Empresarial
+
+/* 
+nombreEmpresa{
+    year: [salario]
+}
+*/
+
+const empresas = {};
+
+for (persona of salarios) {
+    for (trabajo of persona.trabajos) {
+        if(!empresas[trabajo.empresa]){
+            empresas[trabajo.empresa] = {};
+        }
+
+        if(!empresas[trabajo.empresa][trabajo.year]){
+            empresas[trabajo.empresa][trabajo.year] = [];
+        }
+
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+}
+console.log(empresas);
